b4341e314ce3193aa857c41e488b37237b0e80db
saberhosneydev <ssber277@gmail.com>
2020-08-16 15:07:29 +0200

    woef woef, we finally made it to phase three

--- scrapper/index.js
+++ scrapper/index.js
@@ -6,41 +6,95 @@ const { getProductsUrls } = require("./modules/productsUrls");
 const baseURL = "https://www.materielelectrique.com";
 
 if (process.argv[2] == "--task=categories") {
-    global.currentWorkingUrls = new Proxy([], {
-        get: function (target, property) {
-            return target[property];
-        },
-        set: function (target, property, value) {
-            categoriesSubLinks(baseURL + value);
-            target[property] = value;
-            return true;
-        }
-    });
-    getCategories(baseURL);
+	global.currentWorkingUrls = new Proxy([], {
+		get: function (target, property) {
+			return target[property];
+		},
+		set: function (target, property, value) {
+			categoriesSubLinks(baseURL + value);
+			target[property] = value;
+			return true;
+		}
+	});
+	getCategories(baseURL);
 }
 if (process.argv[2] == "--task=products") {
-    global.currentWorkingUrls = new Proxy([], {
-        get: function (target, property) {
-            return target[property];
-        },
-        set: function (target, property, value) {
-            getProductsUrls(baseURL + value);
-            target[property] = value;
-            return true;
-        }
-    });
-    fs.readFile('./csv/urls.csv', { encoding: "utf-8" }, function (err, data) {
-        if (!err) {
-            const processed = data.split(',');
-            processed.forEach(url => {
-                let filterd = url.replace("\n", "");
-                setTimeout(() => {
-                    currentWorkingUrls.push(filterd);
-                }, 10000);
-            });
-        } else {
-            console.log(err);
-        }
-    });
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
 
+
+	global.currentWorkingUrls = new Proxy([], {
+		get: function (target, property) {
+			return target[property];
+		},
+		set: function (target, property, value) {
+			getProductsUrls(baseURL + value);
+			target[property] = value;
+			return true;
+		}
+	});
+	fs.readFile('./csv/urls.csv', { encoding: "utf-8" }, function (err, data) {
+		if (!err) {
+			const processed = data.split(',');
+			let i = 0;
+			let delay = setInterval(()=> {
+				let url = processed[i];
+				let filterd = url.replace("\n", "");
+				currentWorkingUrls.push(filterd);
+				if (i >= 1901) {
+					clearInterval(delay);
+				}else {
+					i +=1;
+				}
+			}, 2000);
+			// processed.forEach(url => {
+			// 	let filterd = url.replace("\n", "");
+			// 	currentWorkingUrls.push(filterd);
+			// 	sleep(2000);
+			// });
+		} else {
+			console.log(err);
+		}
+	});
+}
+if (process.argv[2] == "--task=data") {
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+
+	global.currentWorkingUrls = new Proxy([], {
+		get: function (target, property) {
+			return target[property];
+		},
+		set: function (target, property, value) {
+			getProductsUrls(baseURL + value);
+			target[property] = value;
+			return true;
+		}
+	});
+	fs.readFile('./csv/urls.csv', { encoding: "utf-8" }, function (err, data) {
+		if (!err) {
+			const processed = data.split(',');
+			let i = 0;
+			let delay = setInterval(()=> {
+				let url = processed[i];
+				let filterd = url.replace("\n", "");
+				currentWorkingUrls.push(filterd);
+				if (i >= 1901) {
+					clearInterval(delay);
+				}else {
+					i +=1;
+				}
+			}, 2000);
+			// processed.forEach(url => {
+			// 	let filterd = url.replace("\n", "");
+			// 	currentWorkingUrls.push(filterd);
+			// 	sleep(2000);
+			// });
+		} else {
+			console.log(err);
+		}
+	});
 }
