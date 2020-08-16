bcaeb3441fc9ad2ed997f4b7a62dcc17517eac3b
saberhosneydev <ssber277@gmail.com>
2020-08-16 18:46:49 +0200

    sucks this just sucks

--- scrapper/index.js
+++ scrapper/index.js
@@ -4,7 +4,7 @@ global.cheerio = require("cheerio");
 const { getCategories, categoriesSubLinks } = require("./modules/categoriesUrls");
 const { getProductsUrls } = require("./modules/productsUrls");
 const { getProductsData } = require("./modules/productsData");
-const baseURL = "https://www.materielelectrique.com";
+global.baseURL = "https://www.materielelectrique.com";
 
 if (process.argv[2] == "--task=categories") {
 	global.currentWorkingUrls = new Proxy([], {
@@ -55,35 +55,36 @@ if (process.argv[2] == "--task=products") {
 	});
 }
 if (process.argv[2] == "--task=data") {
-	function sleep(ms) {
-		return new Promise(resolve => setTimeout(resolve, ms));
-	}
-	global.currentWorkingUrls = new Proxy([], {
-		get: function (target, property) {
-			return target[property];
-		},
-		set: function (target, property, value) {
-			getProductsData(baseURL + value);
-			target[property] = value;
-			return true;
-		}
-	});
-	fs.readFile('./csv/productsUrls.csv', { encoding: "utf-8" }, function (err, data) {
-		if (!err) {
-			const processed = data.split(',');
-			let i = 0;
-			let delay = setInterval(()=> {
-				let url = processed[i];
-				let filterd = url.replace("\n", "");
-				currentWorkingUrls.push(filterd);
-				if (i >= processed.length) {
-					clearInterval(delay);
-				}else {
-					i +=1;
-				}
-			}, 2000);
-		} else {
-			console.log(err);
-		}
-	});
+	getProductsData(baseURL + '/prise-prog-mosaic-droite-blanc-p-4560.html')
+	// function sleep(ms) {
+	// 	return new Promise(resolve => setTimeout(resolve, ms));
+	// }
+	// global.currentWorkingUrls = new Proxy([], {
+	// 	get: function (target, property) {
+	// 		return target[property];
+	// 	},
+	// 	set: function (target, property, value) {
+	// 		getProductsData(baseURL + value);
+	// 		target[property] = value;
+	// 		return true;
+	// 	}
+	// });
+	// fs.readFile('./csv/productsUrls.csv', { encoding: "utf-8" }, function (err, data) {
+	// 	if (!err) {
+	// 		const processed = data.split(',');
+	// 		let i = 0;
+	// 		let delay = setInterval(()=> {
+	// 			let url = processed[i];
+	// 			let filterd = url.replace("\n", "");
+	// 			currentWorkingUrls.push(filterd);
+	// 			if (i >= processed.length) {
+	// 				clearInterval(delay);
+	// 			}else {
+	// 				i +=1;
+	// 			}
+	// 		}, 2000);
+	// 	} else {
+	// 		console.log(err);
+	// 	}
+	// });
 }
--- scrapper/modules/productsData.js
+++ scrapper/modules/productsData.js
@@ -1,15 +1,27 @@
 const getProductsData = async function (url) {
-    const result = await axios.get(url);
-    const $ = cheerio.load(result.data);
-    let MREM = $(".product_infos").html().match(/(<\/b>(.*?)<br>)/g);
-    let name = $("#product h1").text();
-    let description = $("#description").text();
-    let text = $(".price").text().replace(/TTC/g, "");
-    let query = `${MREM[1].replace(/<\/b>\s:\s|<br>/g, "")},${name},${description},${price},Images,${MREM[0].replace(/<\/b>\s|<br>/g, "")},${MREM[2].replace(/<\/b>\s|<br>/g, "")},Price,Category,Sub-category,Sub-category`;
-        fs.appendFile(`./csv/finalResult.csv`, $(element).attr('href') + "," + "\n", (error) => {
-            if (error)
-                throw error;
-        });
+		console.log("Hi");
+	const result = await axios.get(url);
+	console.log("1");
+	const $ = cheerio.load(result.data);
+	console.log("2");
+	let EAN = $(".product_infos").html().match(/(<\/b>\s:\s[0-9]+<br>)/g);
+	let RB = $("#product > div > h2").text().split(" - ");
+	let name = $("#product h1").text();
+	let description = $("#description").text();
+	let price = $(".price").text().replace(/TTC/g, "");
+	let image = $(".thumb_pic").attr('src');
+	console.log("3");
+	let query = `${(RB[0])?RB[0]:$("#product > div > h2").text()},${name},${description},${price},${baseURL + image},${(RB[1]) ? RB[1]: "NaN"},${(EAN[0]) ? EAN[0].replace(/<\/b>\s:\s|<br>/g, ""): "NaN"}`;
+	// console.log(query);
+	let breadcrumb = $(".breadcrumb li a span").each(function (i, elem){
+			console.log("4");
+	});
+	console.log("5");
+	fs.appendFile(`./csv/finalResult.csv`, query + "\n", (error) => {
+		if (error)
+			throw error;
+	});
+	console.log("Done");
 
 }
 exports.getProductsData = getProductsData;
